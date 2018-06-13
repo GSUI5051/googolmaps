589dead8fc008db309686af99c2e71ebed323eb1
koteitan <ukaibutton@mac.com>
2018-06-14 01:11:15 +0900

    imported from quantumtrump
    https://github.com/koteitan/quantumtrump 37e5b8a1

---
+++ image.js
@@ -0,0 +1,44 @@
+// image.js
+// images list of tiles
+var tilelist={
+  //file,    number
+  {"pz1.png",4},
+  {"pz2.png",4},
+  {"pz3.png",4},
+  {"pz4.png",4},
+  {"pz5.png",2},
+  {"pz6.png",4},
+  {"pz7.png",4},
+  {"pz8.png",4},
+  {"pz9.png",4},
+  {"sg1.png",4},
+  {"sg2.png",4},
+  {"sg3.png",4},
+  {"sz1.png",4},
+  {"sz2.png",4},
+  {"sz3.png",4},
+  {"sz4.png",4},
+  {"sz5.png",2},
+  {"sz6.png",4},
+  {"sz7.png",4},
+  {"sz8.png",4},
+  {"sz9.png",4},
+  {"wz1.png",4},
+  {"wz2.png",4},
+  {"wz3.png",4},
+  {"wz4.png",4},
+  {"wz5.png",2},
+  {"wz6.png",4},
+  {"wz7.png",4},
+  {"wz8.png",4},
+  {"wz9.png",4},
+  {"kz1.png",4},
+  {"kz2.png",4},
+  {"kz3.png",4},
+  {"kz4.png",4},
+  {"ap5.png",2},
+  {"as5.png",2},
+  {"aw5.png",2}
+};
+var backtile="up1.png"; // image of back of tile
+
