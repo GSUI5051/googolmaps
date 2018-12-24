df9a78cde8eecb376e873ad47b9947d3f4687bb2
koteitan <koteitan@bach.localdomain>
2018-12-25 01:34:42 +0900

    delete images

--- main.js
+++ main.js
@@ -14,26 +14,7 @@ var procAll=function(){ //main loop
 }
 
 //physics
-var tiles=tilelist.length; //number of tiles
-var pos; // pos[i] = position of i th tile
-var sur; // sur[i] = surface of i th tile={0:back / 1:surface}
-var dep; // dep[i] = i th depth is dep[i] (0 is top, tiles is bottom)
-var gra; // gra[i] = graphics of i th tile
-var ww; // canvas size
 var initGame=function(){
-  pos =new Array(tiles);
-  sur =new Array(tiles);
-  tile=new Array(tiles);
-  dep =new Array(tiles);
-  for(var t=0;t<tiles;t++){
-    pos[t]=new Array(2);
-    pos[t][0]=Math.floor(Math.random()*(ww[0]-tileww[0]));
-    pos[t][1]=Math.floor(Math.random()*(ww[1]-tileww[1]));
-    sur[t]=0;
-    dep[t]=t;
-    tile[t]=t;
-  }
-//  shuffle(tile);
 }
 
 //draw -------------------
@@ -42,28 +23,19 @@ var isRequestedDraw=true;
 var can;
 var ctx;
 var initDraw=function(){
-  gra = new Image();
-  gra.src = tilefile + "?" + new Date().getTime();  //renderer
+//  gra = new Image();
+//  gra.src = tilefile + "?" + new Date().getTime();  //renderer
   can = document.getElementById("canvas");
   if(!can||!can.getContext) return false;
   ctx = can.getContext('2d');
   ww=[can.width, can.height];
 }
 var procDraw=function(){
-  ctx.fillStyle = '#008800'; //green
+  ctx.fillStyle = '#000000'; //black
   ctx.clearRect(0, 0, ww[0]-1, ww[1]-1);
-  if(gra.complete){
-    for(var t=tiles-1;t>=0;t--){ // from bottom
-      var dt=dep[t]; // convert depth into tile index
-      var y0=tileww*tilelist[0][0];
-      ctx.drawImage(gra,
-         tileww [0]*tilelist[dt][0], tileww [1]*tilelist[dt][1],
-         tileww [0],                 tileww [1],
-         pos[dt][0],                 pos[dt][1],
-         tileww [0],                 tileww [1]);
-    };
+//  if(gra.complete){
+//    };
     isRequestedDraw = false;
-  }
 }
 //event---------------------
 var isKeyTyping;
@@ -80,37 +52,15 @@ var initEvent = function(){
 //  window.onkeydown       = addEvent;
 };
 
-var hittile=-1; // index of the hit tile
-var hitpos=[-1,-1];// position of hit
 var handleMouseDown = function(){
   //seek pressed tile
-  for(var t=0;t<tiles;t++){
-    var dt=dep[t];
-    var l=[mouseDownPos[0]-pos[dt][0],mouseDownPos[1]-pos[dt][1]];
-    if(0<l[0] && l[0]<tileww[0] && 
-       0<l[1] && l[1]<tileww[1]){
-      //hit
-      hittile=dt;
-      hitpos[0]=mouseDownPos[0]-pos[dt][0];
-      hitpos[1]=mouseDownPos[1]-pos[dt][1];
-      //move to top
-      for(var t2=t-1;t2>=0;t2--) dep[t2+1]=dep[t2];
-      dep[0]=hittile;
-      break;
-    }
-  }
+//    var l=[mouseDownPos[0]-pos[dt][0],mouseDownPos[1]-pos[dt][1]];
   isRequestedDraw = true;
 }
 var handleMouseDragging = function(){
-  if(hittile!=-1){
-    var h=hittile;
-    pos[h][0]=mousePos[0]-hitpos[0];
-    pos[h][1]=mousePos[1]-hitpos[1];
-  }
   isRequestedDraw = true;
 }
 var handleMouseUp = function(){
-  hittile=-1;
   isRequestedDraw = true;
 }
 
