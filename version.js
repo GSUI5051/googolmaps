4df7fc4f38c012710b67e275dfff26ce7d8a3279
koteitan <ukaibutton@mac.com>
2018-12-25 01:17:50 +0900

    remove ordinal Mahjong

--- version.js
+++ version.js
@@ -1,7 +1,7 @@
 document.getElementById("versiondisplay").innerHTML = "0.1";
 document.getElementById("changelog").innerHTML = 
 "<ul>"+
-"<li>2018/06/14 1:13:16 start implementation</li>"+
+"<li>2018/12/25 01:15:00 start implementation</li>"+
 "</ul>";
 document.getElementById("knownbugs").innerHTML = 
 "<ul>"+
